1c08be38f07569f2fdaa449c1576f09f0fda8524
Nikolaou Nikos <ict.edu1@gmail.com>
2022-05-05 16:35:58 +0300

    Finished Lesson 04

---
+++ ClassroomPlanner/src/ClassroomPlannerApp.Shared/Responses/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassroomPlannerApp.Shared.Responses
+{
+    public class ApiErrorResponse
+    {
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+        public bool isSuccess { get; set; }
+
+    }
+}
